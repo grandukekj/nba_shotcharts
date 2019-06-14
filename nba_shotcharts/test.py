0f101c80faa2fa09e9761552f6022a51874c7576
Kwangjun Lee <grandukekj@gmail.com>
2019-06-14 22:23:59 +0200

    absolute imports

--- nba_shotcharts/test.py
+++ nba_shotcharts/test.py
@@ -1,7 +1,9 @@
-import nba_player_stat as nps
-import plot_shotchart as ps
+# from. nba_player_stat import nps
+# from. plot_shotchart import ps
+from nba_shotcharts.nba_player_stat import NBA_player_stat
+from nba_shotcharts.plot_shotchart import plot_shotchart
 
-james_harden = nps.NBA_player_stat('james harden', 2019)
+james_harden = NBA_player_stat('james harden', 2019)
 jh_RS_shot_df, jh_PO_shot_df = james_harden.shot_chart_to_pandas()
-search_shot_fig = ps.plot_shotchart(jh_RS_shot_df, month=3, HW='home')
+search_shot_fig = plot_shotchart(jh_RS_shot_df, month=3, HW='home')
 search_shot_fig.savefig('../shot_test.png')
